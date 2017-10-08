39268ab55033bf6e599b1f256e8b77afd4e77351
CodeWithSufiyan <sufiyanbudye7@gmail.com>
2017-10-08 21:34:07 +0530

    Added API to validate for empty txt

--- Validator/Scripts/validator.js
+++ Validator/Scripts/validator.js
@@ -1,1 +1,48 @@
-// Object name: Validator
+// constructor: validate
+
+(function ($) {
+
+    $.fn.validate = function (options) {
+
+        var rules = $.extend({
+            isIncorrect: function () { },
+            isNumber: false,
+            isNotNumber: false,
+            ViewMsgSuccess: function(sMsg){
+                this.ViewAlert(sMsg);
+            },
+            ViewMsgFailure: function(sMsg){
+                this.ViewAlert(sMsg);
+            },
+        }, options);
+
+        var Alert = {
+            ViewAlert: function(sMsg){
+                var elAlertDanger = 
+                $("<div class='alert alert-danger alert-dismissible fade in alert-message' role='alert' style='position:absolute; z-index:1;'>\
+                    <button class='close' aria-label='Close' type='button' data-dismiss='alert'><span aria-hidden='true'>X</span></button>\
+                        <strong>"+ sMsg + "</strong>\
+                </div>");
+                $("#divAlert").html(elAlertDanger).alert();
+                setTimeout(function () {
+                    $("#divAlert").alert('close');
+                }, 4000);
+            }
+        }
+
+        return this.each(function () {
+            if(rules.isIncorrect($(this))){
+                rules.ViewMsgFailure.call(Alert,"Invalid");
+            }
+        });
+    };
+
+    
+    $("#btnValidateEmptiness").click(function(){
+        $("#txtEmptiness").validate({
+            isIncorrect: function(el){
+                return el.val()=="";
+            }
+        });
+    });
+})(jQuery);
