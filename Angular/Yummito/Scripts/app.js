d3b4a8a6e726452ae84c7962c79145ee585c8d7a
CodeWithSufiyan <sufiyanbudye7@gmail.com>
2017-10-15 23:49:35 +0530

    Added list view

---
+++ Angular/Yummito/Scripts/app.js
@@ -0,0 +1,22 @@
+// constructor: validate
+
+(function ($) {
+
+    var oApp = angular.module('Yummito', []);
+    oApp.controller('YummitoController', function ($scope) {
+        var arrFood = ['wada-pav', 'bhaji', 'tea', 'missal', 'pohe'];
+        $scope.arrFood = arrFood;
+        $scope.addItem = function () {
+            if (!$scope.txtItem) {
+                return;
+            }
+            $scope.arrFood.push($scope.txtItem);
+            $scope.txtItem = "";
+        }
+
+        $scope.removeItem = function ($index) {
+            $scope.arrFood.splice($index, 1);
+        }
+    });
+
+})(jQuery);
