5048e07a41b85c00915ed988f869559d603ad716
CodeWithSufiyan <sufiyanbudye7@gmail.com>
2017-10-27 00:29:28 +0530

    Sufi: Added index.html & Added Snippet for node 'http', 'net', 'url' connection

--- Node/ChitChatWithNode/Scripts/chat-client.js
+++ Node/ChitChatWithNode/Scripts/chat-client.js
@@ -1,2 +1,74 @@
 
 // Initialize TCP Protocol
+// var http = require('http'), 
+//     fs = require('fs');
+
+// fs.readFile('./../index.html', function(err, data){
+//     http.createServer(function(req, res){
+//         res.writeHead(200, {'Content-Type':'text/html'}); // text/plain
+//         res.write(data);
+//         res.end();
+//     }).listen(8017, "127.0.0.1");
+// })
+
+
+// var net = require('net');
+// var chatServer = net.createServer();
+// chatServer.on('connection', function(client){
+//     client.write("Hi");
+
+//     client.end();
+// }).listen(8017)
+
+const http = require('http');
+const net = require('net');
+const url = require('url');
+
+// Create an HTTP tunneling proxy
+const proxy = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('okay');
+});
+proxy.on('connect', (req, cltSocket, head) => {
+  // connect to an origin server
+  const srvUrl = url.parse(`http://${req.url}`);
+  const srvSocket = net.connect(srvUrl.port, srvUrl.hostname, () => {
+    cltSocket.write('HTTP/1.1 200 Connection Established\r\n' +
+                    'Proxy-agent: Node.js-Proxy\r\n' +
+                    '\r\n');
+    srvSocket.write(head);
+    srvSocket.pipe(cltSocket);
+    cltSocket.pipe(srvSocket);
+  });
+});
+
+// now that proxy is running
+proxy.listen(1337, '127.0.0.1', () => {
+
+  // make a request to a tunneling proxy
+  const options = {
+    port: 1337,
+    hostname: '127.0.0.1',
+    method: 'CONNECT',
+    path: 'www.google.com:80'
+  };
+
+  const req = http.request(options);
+  req.end();
+
+  req.on('connect', (res, socket, head) => {
+    console.log('got connected!');
+
+    // make a request over an HTTP tunnel
+    socket.write('GET / HTTP/1.1\r\n' +
+                 'Host: www.google.com:80\r\n' +
+                 'Connection: close\r\n' +
+                 '\r\n');
+    socket.on('data', (chunk) => {
+      console.log(chunk.toString());
+    });
+    socket.on('end', () => {
+      proxy.close();
+    });
+  });
+});
